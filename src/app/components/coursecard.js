c6d2a506a900ed498441f0f109225b95018bf17a
Prakrit Chauhan <prakritchauhan@Prakrits-MacBook-Pro.local>
2025-09-13 17:08:44 -0500

    init

--- src/app/components/coursecard.js
+++ src/app/components/coursecard.js
@@ -9,10 +9,25 @@ const data = [
   }
 ];
 
-const CourseCard = () => {
+const CourseCard = (props) => {
   return (
-    <div>
-      
+    <div className=" bg-white shadow-md rounded-lg overflow-hidden m-10">
+        <div>
+          <h3 className="text-blue-600 px-8 py-2 pt-6 font-bold text-2xl">
+            {props.courseCode}
+          </h3>
+          <h4 className="px-8 py-2 text-gray-800 font-semibold text-xl">
+            {props.title}
+          </h4>
+          <h5 className="px-8 py-2 text-gray-600">
+            {props.description}
+          </h5>
+          <div className="bg-green-300 w-fit shadow-md rounded-lg overflow-hidden ml-7 m-2 mb-5">
+            <h5 className="px-2 text-shadow-green-900">
+              {props.creditHours} Credit Hours
+            </h5>
+          </div>
+        </div>
     </div>
   );
 };
--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Hero() {
   return (
     <div>
-
+      <div className="flex justify-center items-center py-10 px-10 text-sm bg-sky-100">
+        Maxing our GPA. One Class at a Time.
+      </div>
     </div>
   );
 }
